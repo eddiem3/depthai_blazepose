4626d24346c48e052f8549f6fb630bdef96c3c7d
Eddie Massey III <eem33@case.edu>
2023-03-19 21:38:09 -0400

    draft of melodic publisher

--- demo.py
+++ demo.py
@@ -4,24 +4,20 @@ from BlazeposeRenderer import BlazeposeRenderer
 from BlazeposeDepthai import BlazeposeDepthai
 
 
-import rclpy
-from rclpy.node import Node
+import rospy
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 
 
 
 
-class BlazePosePublisher(Node):
-    def __init__(self):
-        super().__init__('pose_publisher')
-        self.publisher_ = self.create_publisher(Image,'pose_img', 10)
-        timer_period = 0.1
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.br = CvBridge()
+def pose_publisher()
+
+        publisher = rospy.Publisher('pose_img', Image)
+        br = CvBridge()
 
 
-        self.tracker = BlazeposeDepthai(input_src='rgb', 
+        tracker = BlazeposeDepthai(input_src='rgb', 
                                    pd_model=None,
                                    lm_model=None,
                                    smoothing=True,   
@@ -33,48 +29,42 @@ class BlazePosePublisher(Node):
                                    stats=True,
                                    trace=False)
 
-        self.renderer = BlazeposeRenderer(
-            self.tracker, 
+        renderer = BlazeposeRenderer(
+            tracker, 
             show_3d=None, 
             output=None)
 
-    def timer_callback(self):
+
 
         while True:
             # Run blazepose on next frame
-            frame, body = self.tracker.next_frame()
+            frame, body = tracker.next_frame()
             if frame is None: break
 
             # Draw 2d skeleton
-            frame = self.renderer.draw(frame, body)
-            key = self.renderer.waitKey(delay=1)
+            frame = renderer.draw(frame, body)
+            key = renderer.waitKey(delay=1)
             if key == 27 or key == ord('q'):
                 break
 
-            self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
+            publisher.publish(br.cv2_to_imgmsg(frame))
         
-
-
-def main(args=None):
-    rclpy.init(args=args)
-
-    blazepose = BlazePosePublisher()
-
-    rclpy.spin(blazepose)
-
-    blazepose.renderer.exit()
-    blazepose.tracker.exit()
-
-
-    minimal_publisher.destroy_node()
-    rclpy.shutdown()
 
     
 
 
 if __name__ == '__main__':
-    main()
-        
+    rospy.init_node('pose_estimator')
+    
+    try:
+        pose_publisher()
+    except rospy.ROSInterruptException:
+        pass
+
+    blazepose.renderer.exit()
+    blazepose.tracker.exit()
+
+
 
 
 
